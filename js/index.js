3b47988f25e494233cfa599cc7902e22b9ca22aa
Alon Gugig <Alongugig@gmail.com>
2022-11-01 17:06:42 +0200

    Resolved conflicts

--- js/index.js
+++ js/index.js
@@ -1,39 +1,41 @@
 const queryString = window.location.search;
 const fullResultsUrl = "https://script.google.com/macros/s/AKfycbyKWmvN7Abj12obdm3F0q15Xxi5KILdnqW0Lu5xHc-NSJTAQB2W1mk9ZLjzV2hT6Mbi/exec?national=0"
 const nationalResultsUrl = "https://script.google.com/macros/s/AKfycbyKWmvN7Abj12obdm3F0q15Xxi5KILdnqW0Lu5xHc-NSJTAQB2W1mk9ZLjzV2hT6Mbi/exec?national=1"
+
 const urlParams = new URLSearchParams(queryString);
 let ln = urlParams.get('ln') ? urlParams.get('ln') : "he";
 const showBorders = urlParams.get('border') ? urlParams.get('border') : 0;
 const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
 let results = 0
+     
 var map = new maplibregl.Map({
-  container: "map",
-  style: {
+container: "map",
+style: {
     version: 8,
     glyphs: "https://bogind.github.io/glfonts/{fontstack}/{range}.pbf",
     sources: {},
     layers: [
-      {
+    {
         id: "background",
         type: "background",
         layout: {
-          visibility: "visible",
+        visibility: "visible",
         },
         paint: {
-          "background-color": {
+        "background-color": {
             stops: [
-              [6, "rgba(252, 247, 229, 1)"],
-              [10, "rgba(252, 247, 229, 1)"],
-              [14, "rgba(246, 241, 229, 1)"],
-              [15, "rgba(246, 241, 229, 1)"],
+            [6, "rgba(252, 247, 229, 1)"],
+            [10, "rgba(252, 247, 229, 1)"],
+            [14, "rgba(246, 241, 229, 1)"],
+            [15, "rgba(246, 241, 229, 1)"],
             ],
-          },
         },
-      },
+        },
+    },
     ],
-  }, // stylesheet location
-  center: [35.078096, 31.4411522], // starting position [lng, lat]
-  zoom: 7, // starting zoom
+}, // stylesheet location
+center: [35.078096, 31.4411522], // starting position [lng, lat]
+zoom: 7, // starting zoom
 });
 let israelGJ = {
     "type": "FeatureCollection",
@@ -69,7 +71,6 @@ async function onMapLoad(){
             }
         polygonFilter = ['!=', 'name', 'dissolved']
     }
-
     map.addLayer({
         id: "israelBG",
         type: "fill",
@@ -78,7 +79,6 @@ async function onMapLoad(){
         paint: polygonPaint,
         filter: polygonFilter,
     })
-    
     if(showBorders == 1){
         map.addLayer({
             'id': 'israelBG-line',
@@ -96,6 +96,7 @@ async function onMapLoad(){
     
     loadBG()
 }
+
 async function loadBG(){
     const BGresponse = await fetch('israel.fgb');
     for await (let feature of flatgeobuf.deserialize(BGresponse.body, undefined)) {
@@ -118,118 +119,41 @@ async function loadBG(){
             partyColor = allResponses[1]
             results = runCalc(allResponses[2])
             setsGJ = allResponses[3]
+
             addLayer()
             addNationalResultsPlot()
 
           })
 }
-async function loadBG() {
-  const BGresponse = await fetch("israel.fgb");
-  for await (let feature of flatgeobuf.deserialize(
-    BGresponse.body,
-    undefined
-  )) {
-    israelGJ.features.push(feature);
-    let sourceObject = map.getSource("israelBG");
-    sourceObject.setData(israelGJ);
-  }
+map.on('load',onMapLoad)
 
-  Promise.all([
-    fetch("elections.json").then((value) => value.json()),
-    fetch("parties.json").then((value) => value.json()),
-    fetch("sets4.geojson").then(value => value.json()),
-  ]).then((allResponses) => {
-    results2021 = allResponses[0];
-    partyColor = allResponses[1];
-    setsGJ = allResponses[2]
-    addLayer();
-  });
-}
-map.on("load", onMapLoad);
 
-let partyColor;
+
+
+let partyColor
 let results2021;
 
-function addPartiesInfo(geojson, partyColor) {
-  geojson.features.forEach((feature) => {
-    var partyColorForFeature = partyColor[feature.properties.max_party];
 
-    if (partyColorForFeature) {
-      feature.properties.partyColor = partyColorForFeature.Color;
-      feature.properties.partyName = tr(feature.properties.max_party, ln);
-      feature.properties.cityVotingHeight =
-        feature.properties.votingPercentage * 500;
+function addLayer(){
+
+    var geojson = addPartiesInfo(results2021.citiesData.results, partyColor);
+    map.addSource('results', {
+    'type': 'geojson',
+    'data': geojson
+    });
+
+    function addPartiesInfo(geojson, partyColor) {
+        geojson.features.forEach((feature) => {
+        var partyColorForFeature = partyColor[feature.properties.max_party];
+
+        if (partyColorForFeature) {
+            feature.properties.partyColor = partyColorForFeature.Color;
+            feature.properties.partyName = tr(feature.properties.max_party,ln);
+            feature.properties.cityVotingHeight = feature.properties.votingPercentage * 500
         //feature.properties.votingPercentage = feature.properties.electionsResults.כשרים/feature.properties.electionsResults.בזב
+
     }
   });
-
-  return geojson;
-}
-
-function addLayer() {
-  var geojson = addPartiesInfo(results2021.citiesData.results, partyColor);
-  map.addSource("results", {
-    type: "geojson",
-    data: geojson,
-  });
-
-  map.addLayer({
-    id: "results",
-    type: "fill-extrusion",
-    source: "results",
-    layout: {},
-    paint: {
-      // Get the `fill-extrusion-color` from the source `color` property.
-      "fill-extrusion-color": ["get", "partyColor"],
-
-      // Get `fill-extrusion-height` from the source `height` property.
-      "fill-extrusion-height": ["get", "cityVotingHeight"],
-
-      // Get `fill-extrusion-base` from the source `base_height` property.
-      "fill-extrusion-base": 50,
-
-      // Make extrusions slightly opaque to see through indoor walls.
-      "fill-extrusion-opacity": 0.5,
-    },
-  });
-  map.addLayer({
-    id: "labels-symbol",
-    type: "symbol",
-    source: "results",
-    layout: {
-      "text-font": ["Noto Sans Regular"],
-      "text-field": ["get", "areaId"],
-      "text-size": 16,
-      "text-anchor": "bottom",
-      "text-offset": [0, -2],
-    },
-    paint: {
-      "text-color": "black",
-      "text-halo-color": "white",
-      "text-halo-width": 1,
-    },
-    filter: [
-      "in",
-      ["get", "areaId"],
-      [
-        "literal",
-        ["תל אביב יפו", "ירושלים", "חיפה", "אילת", "טבריה", "באר שבע"],
-      ],
-    ],
-  });
-  // map.setLayoutProperty('results', 'text-field', [
-  //     'format',
-  //     ['get', 'areaId'],
-  //     { 'font-scale': 1.2 },
-  //     {
-  //     'font-scale': 0.8,
-  //     'text-font': [
-  //     'literal',
-  //     ['David', 'Arial Unicode MS Regular']
-  //     ]
-  //     }
-  //     ]);
-
 
   return geojson;
 }
@@ -255,8 +179,32 @@ function addLayer() {
         'fill-extrusion-opacity': 0.5
         }
         });  
-       
-    
+        map.addLayer({
+            id: "labels-symbol",
+            type: "symbol",
+            source: "results",
+            layout: {
+              "text-font": ["Noto Sans Regular"],
+              "text-field": ["get", "areaId"],
+              "text-size": 16,
+              "text-anchor": "bottom",
+              "text-offset": [0, -2],
+            },
+            paint: {
+              "text-color": "black",
+              "text-halo-color": "white",
+              "text-halo-width": 1,
+            },
+            filter: [
+              "in",
+              ["get", "areaId"],
+              [
+                "literal",
+                ["תל אביב יפו", "ירושלים", "חיפה", "אילת", "טבריה", "באר שבע"],
+              ],
+            ],
+          });  
+
     addInteractions()
 }
 
@@ -284,13 +232,55 @@ function addInteractions(){
                 orientation: 'h',
                 width: 0.5,
                 type:'bar'
-                }
-               }
-      }
 
+            }
+            data.push(trace)
+        }
+        
+        
+        var layout = {barmode: 'stack',
+                        height:200,
+                        showlegend: false,
+                        yaxis:{
+                            showline:false,
+                            showgrid:false
+                        },
+                        xaxis:{
+                            showline:false,
+                            showgrid:false,
+                            zeroline:false
 
-
-
+                        },
+                        margin:{
+                            t:0,
+                            b:0,
+                            l:0,
+                            r:0
+                        }};
+         
+        
+         
+        new maplibregl.Popup({maxWidth:'300px'})
+            .setLngLat(center.geometry.coordinates)
+            .setHTML(description)
+            .addTo(map);
+        
+        setDirection(ln)
+        console.log(data)
+        Plotly.newPlot('plot', data, layout,{scrollZoom: false, displayModeBar: false});
+        });
+         
+        // Change the cursor to a pointer when the mouse is over the places layer.
+        map.on('mouseenter', 'results', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+         
+        // Change it back to a pointer when it leaves.
+        map.on('mouseleave', 'results', function () {
+            map.getCanvas().style.cursor = '';
+        });
+        
+}
 
 function setDirection(ln){
     if(ln != "he" || "ar" ){
@@ -310,7 +300,6 @@ function addNationalResultsPlot(){
             x.push(element.partyName)
             y.push(element.totalMandates)         
         }
-
         //plotData({x:[].push(element.partyName),y:[].push(element.votes)})
         
     }
@@ -351,29 +340,66 @@ class displayNationtalScore {
         const slider = this.slider
         const contants = this.contants
         
+        //var result = Object.keys(results).map((key) => [key, results[key]]);
+
         
         this.nationtalScore.addEventListener("click", function() {
 
             if (window.getComputedStyle(slider).getPropertyValue("display") == "none") {
-                console.log("up")
                 slider.classList.toggle("slide-down")
-                slider.style.display = "block";
+
+                contants.style.display = "block";
               }
-              else if (window.getComputedStyle(slider).getPropertyValue("display") == "block") {
-                console.log("down")
-                slider.style.display = "none";
+              else if (window.getComputedStyle(contants).getPropertyValue("display") == "block") {
+                contants.style.display = "none";
               }
         
-             
-                
-                // showNationalResults(contants)
             
         });
 
         
         return this.container;
     }
+    // onAdd(map){
+    //     this.map = map;
+        
+    //     this.container = document.createElement('div');
+    //     this.container.className = 'custom-control-class maplibregl-ctrl mapboxgl-ctrl';
 
+    //     this.nationtalScore = document.createElement('button');
+    //     this.nationtalScore.className = "nationalResultsBtn"
+    //     this.nationtalScore.textContent = tr("nationalResults",ln)
+
+    //     this.container.appendChild(this.nationtalScore)
+
+    //     this.slider = document.createElement("div")
+    //     this.slider.id = "slider"
+    //     this.slider.className = "slide-up"
+
+    //     this.container.appendChild(this.slider)
+
+    //     this.filler = document.createElement("div")
+
+    //     this.slider.appendChild(this.filler)
+
+    //     this.contants = document.createElement("p")
+    //     this.contants.className = "contents"
+    //     this.contants.textContent = "teset"
+
+    //     this.slider.appendChild(this.contants)
+
+    //     const slider = this.slider
+    //     this.nationtalScore.addEventListener("click", function() {
+    //             //let silder = document.getElementById("slider");
+        
+    //             slider.classList.toggle("slide-down")
+            
+    //     //     //let results = calcMandates()
+    //     });
+
+        
+    //     return this.container;
+    // }
     onRemove(){
       this.container.parentNode.removeChild(this.container);
       this.map = undefined;
@@ -384,7 +410,6 @@ class displayNationtalScore {
   let myDisplayNationtalScore = new displayNationtalScore();
 
   map.addControl(myDisplayNationtalScore);
-
 
   class languageSelectionButtons {
     onAdd(map) {
@@ -414,22 +439,13 @@ class displayNationtalScore {
       return this.container;
 
     }
-    function changeLanguge(clickablearea, languageChange) {
-      clickablearea.addEventListener("click", () => {
-        ln = languageChange;
-        setDirection(ln);
-      });
+    onRemove(){
+      this.container.parentNode.removeChild(this.container);
+      this.map = undefined;
     }
-
-    return this.container;
   }
-  onRemove() {
-    this.container.parentNode.removeChild(this.container);
-    this.map = undefined;
-  }
-}
-
 
   let myLanguageSelectionButtons = new languageSelectionButtons();
 
   map.addControl(myLanguageSelectionButtons,position = 'top-left');
+
